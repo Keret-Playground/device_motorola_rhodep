4236d9d64f353f7dcc94af8f51f9d2567a33a2b1
Demon Singur <demonsingur@gmail.com>
2018-07-14 14:37:51 +0000

    sm7325-common: do not build useless packages
    Change-Id: I72076113083308e26573289923f5d3c00a8a6b44

--- common.mk
+++ common.mk
@@ -215,9 +215,7 @@ $(foreach f,$(wildcard $(LOCAL_PATH)/rootdir/bin/*.sh),\
 # IPACM
 PRODUCT_PACKAGES += \
     ipacm \
-    IPACM_cfg.xml \
-    libipanat \
-    liboffloadhal
+    IPACM_cfg.xml
 
 # IPC router config
 PRODUCT_COPY_FILES += \
@@ -352,8 +350,7 @@ PRODUCT_PACKAGES += \
     libprotobuf-cpp-full \
     libprotobuf-cpp-full-3.9.1-vendorcompat \
     libprotobuf-cpp-lite-3.9.1-vendorcompat \
-    librmnetctl \
-    libxml2
+    librmnetctl
 
 # Sensors
 PRODUCT_PACKAGES += \
