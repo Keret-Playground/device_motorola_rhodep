291a5de200498fe45cae142584fd7305b1899991
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-01-24 14:02:20 +0530

    rhodep: Switch to dot product CPU variant
     * berlin:/ $ cat /proc/cpuinfo | grep -m 1 Features
       Features: fp asimd evtstrm aes pmull sha1 sha2 crc32
       atomics fphp asimdhp cpuid asimdrdm lrcpc dcpop asimddp
    
     * From this, it's evident that our CPU (Snapdragon 778G Plus)
       supports all the features of ARMv8.2-DotProd [1].
       Moreover AOSP sets this arch variant in their generic
       cortex-a55 ART target as well [2]. This should unlock
       some optimizations in different code paths.
    
    [1]: https://en.wikichip.org/wiki/arm/armv8#ARMv8_Extensions_and_Processor_Features
    [2]: https://android.googlesource.com/device/generic/art/+/refs/heads/master/armv8_cortex_a55/BoardConfig.mk#23
    
    Change-Id: I484bb2bcc181c62e29c04bc6e467e5edea8ac0d8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,7 +10,7 @@ DEVICE_PATH := device/motorola/rhodep
 
 # Architecture
 TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-2a
+TARGET_ARCH_VARIANT := armv8-2a-dotprod
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_VARIANT := cortex-a76
 
