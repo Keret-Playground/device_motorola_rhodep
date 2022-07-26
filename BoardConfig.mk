e9915f418381e4ee36b7fb52fdc053e9f6f492d6
Cyber Knight <cyberknight755@gmail.com>
2022-07-26 17:03:19 +0800

    rhodep: Stop enabling memcg
    - We are not on a low-end device to use memcg hence let's drop it.
    
    Change-Id: Ib724fdb67a47dc039e68305461d60bc02468cd5d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -50,7 +50,7 @@ BOARD_BOOT_HEADER_VERSION := 3
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom androidboot.console=ttyMSM0
-BOARD_KERNEL_CMDLINE += androidboot.memcg=1 lpm_levels.sleep_disabled=1
+BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237
 BOARD_KERNEL_CMDLINE += service_locator.enable=1 androidboot.usbcontroller=4e00000.dwc3
 BOARD_KERNEL_CMDLINE += swiotlb=0 loop.max_part=7 cgroup.memory=nokmem,nosocket
