812597efa050e0164585df7d9483bc0c67fe9890
Notkerd69 <29781314+Notkerd69@users.noreply.github.com>
2024-05-21 14:15:30 +0000

    rhodep: Add hardware/motorola to soong namespaces
    Signed-off-by: Notkerd69 <29781314+Notkerd69@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -475,7 +475,8 @@ PRODUCT_SHIPPING_API_LEVEL := 31
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/samsung
+    hardware/samsung \
+    hardware/motorola
 
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
