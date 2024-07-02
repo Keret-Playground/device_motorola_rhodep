8568714fd4a4672f77ce76b436cb277f108de271
Notkerd69 <29781314+Notkerd69@users.noreply.github.com>
2024-07-02 01:09:02 +0000

    rhodep: Android.mk: fixup!
    Change-Id: I80d4c879a8724d8b167cd0423ea3abe5e39a0c0a
    Signed-off-by: Notkerd69 <29781314+Notkerd69@users.noreply.github.com>

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter bangkk corfur penang rhodep cypfr,$(TARGET_DEVICE)),)
+ifeq ($(TARGET_DEVICE),rhodep)
 subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
 $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
 
